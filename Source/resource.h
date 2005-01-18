8267eca7fb2f52371d2323b9f7b10430b62d0471
Alexander Nagiller <filters.virtualdub@chello.at>
2005-01-18 12:00:00 +0000

    Version 1.21
    Version 1.21 (18.01.2005)
    * Added direct mouse filter edit support
    
    Version 1.11 (21.01.2004)
    * An import/export function has been integrated to import/export .amp files
    * A small bug in the weighted mode is now fixed
    
    Version 1.00 (08.01.2004)
    * First release

---
+++ Source/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by gradation.rc
+//
+#define IDPREVIEW                       3
+#define IDIMPORT                        4
+#define IDD_FILTER                      101
+#define IDC_EXPAND                      1000
+#define IDC_THIRD                       1001
+#define IDC_SVALUE                      1005
+#define IDC_VALUE                       1006
+#define IDC_OUTPUTPLUS                  1007
+#define IDC_OUTPUTMINUS                 1008
+#define IDC_INPUTMINUS                  1009
+#define IDC_INPUTPLUS                   1010
+#define IDC_OUTPUTVALUE                 1011
+#define IDC_RESET                       1012
+#define IDC_INVERT                      1013
+#define IDC_GRADCURVE                   1014
+#define IDC_SMOOTH                      1015
+#define IDC_HBORDER                     1016
+#define IDC_VBORDER                     1017
+#define IDC_CHANNEL                     1018
+#define IDC_RGB                         1019
+#define IDC_FULL                        1020
+#define IDC_RGBW                        1021
+#define IDC_OFF                         1022
+#define IDC_FULLW                       1023
+#define IDC_TEST                        1025
+#define IDEXPORT                        1026
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1029
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
